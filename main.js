91d9e044ad0926e8b52f51035abf608940bf2cc9
Eva Halfarova <halfarovaevacz@gmail.com>
2023-01-31 22:06:56 +0100

    zpracovaný úkol

--- main.js
+++ main.js
@@ -1,1 +1,65 @@
 // tady je místo pro náš program
+let nadpis = document.querySelector ("h1")
+nadpis.classList.add ("csstrida")
+
+let ctverecek = document.querySelector ("#ctverecek1")
+
+function zmenBarvu(){
+ctverecek.classList.toggle("mujstyl") }
+
+let tlacitko = document.querySelector (".tlacitko")
+
+
+function zavolejUdalost(udalost){
+    let prvek = udalost.target;
+console.log (prvek)
+console.log (udalost)}
+
+
+let mujPrehravac = document.querySelector ('.mujPrehravac');
+
+function spustAudio(){
+    mujPrehravac.play();
+}
+
+//dú 3  
+function pauseAudio(){
+    mujPrehravac.pause();
+}
+
+function resetAudio(){
+    mujPrehravac.currentTime=0;
+}
+//du 3
+function volumeAudio1(){
+    mujPrehravac.volume = 0;
+}
+
+function volumeAudio2(){
+    mujPrehravac.volume = 0.5;
+}
+
+function volumeAudio3(){
+    mujPrehravac.volume = 1;
+}
+
+//úkol na lekci
+function ztucnit(){
+  let odstavec = document.querySelector ('#odstavec');
+    console.log (odstavec);
+    odstavec.style.fontWeight = 'bold';}
+    
+//dú 1
+function zmenaBarvy(){
+    let odstavec = document.querySelector('#odstavec');
+
+    odstavec.classList.toggle ('odstaveczmenabarvy')
+}
+
+//dú 2
+function zvetsit(){
+    odstavec.classList.add ('zmenapisma');
+}
+
+
+
